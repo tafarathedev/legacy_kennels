41635f0b39722280a7a4ab77c643e6166f5d931c
Tafarathedev <tafaratakaiza19@gmail.com>
2023-01-12 01:36:55 -0800

    d

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
-import { BrowserRouter as Router, Route , Routes, Switch } from 'react-router-dom'
+import { BrowserRouter as Router, Route , Routes} from 'react-router-dom'
 import Products from './pages/Products'
 import ProductDetails from './pages/ProductDetails'
 import About_us from './component/Services'
@@ -19,7 +19,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Router>
       <Routes>
-        <Switch>  
+         
         <Route path="/" element={<App/>} />
         <Route path="/products/product_details" element={<ProductDetails/>} />
         <Route path="/products" element={<Products/>} />
@@ -31,7 +31,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
         <Route path="/blogs"  element={<Blogs/>}/>
         <Route path="/frequently_asked_questions"  element={<FAQs/>}/>
         <Route path="*"  element={<NotFound />} />
-        </Switch>
+        
       </Routes>
     </Router>
   </React.StrictMode>,
